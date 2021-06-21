ab0c92068e606aa950e21dc1c542a55d7b091813
divji <miilliis.studio@gmail.com>
2021-06-21 09:01:33 +0300

    build: add images, fonts to build proccess

--- gulpfile.js
+++ gulpfile.js
@@ -7,12 +7,34 @@ const scripts = require("./tasks/Script");
 const html = require("./tasks/Html");
 const assets = require("./tasks/Public");
 const unusable = require("./tasks/Style");
-// const clean = require('./tasks/Clean');
+const fonts = require("./tasks/Font");
+const images = require("./tasks/Image");
+const clean = require("./tasks/Clean");
+
+exports.convertImages = series(
+	images.images,
+	images.resizeSm,
+	images.resizeMd,
+	images.resizeLg,
+	images.resizeSm2x,
+	images.resizeMd2x,
+	images.resizeLg2x,
+	images.resizeSm3x,
+	images.resizeMd3x,
+	images.resizeLg3x,
+	images.cachemin
+);
+
+exports.convertFonts = parallel(
+	fonts.transform,
+	fonts.transformToWoff2,
+	fonts.ttfRebase
+);
 
 exports.dev = series(
 	series(
-		// this.convertFonts,
-		// this.convertImages,
+		this.convertFonts,
+		this.convertImages,
 		styles.styles,
 		scripts.scripts
 	),
@@ -21,19 +43,16 @@ exports.dev = series(
 );
 
 exports.build = series(
+	clean.cleanPublic,
 	styles.styles,
 	scripts.scripts,
 	unusable.unusable,
 
-	parallel(assets.publicCss, assets.publicJs, html.html)
+	parallel(
+		assets.publicCss,
+		assets.publicJs,
+		html.html,
+		assets.publicFonts,
+		assets.publicImages
+	)
 );
-
-// exports.build = series(
-// 	series(
-// 		clean.cleanPublic,
-// 		unusable.unusable,
-// 		assets.publicFonts,
-// 		assets.publicImages
-// 	),
-// 	parallel(assets.publicCss, assets.publicJs, html.html)
-// );

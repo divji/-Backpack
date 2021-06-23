8fde8bcfec0df6c41e9b16be5483dbe93231ad63
divji <miilliis.studio@gmail.com>
2021-06-23 21:19:43 +0300

    build: change gulp-ttf2towoff to gulp-ttftowoff2 for perfomance reasons

--- tasks/Font.js
+++ tasks/Font.js
@@ -2,7 +2,7 @@ const { src, dest } = require("gulp");
 const { paths } = require("./helpers/PathHelper");
 const changed = require("gulp-changed");
 const fonter = require("gulp-fonter");
-const toWoff2 = require("gulp-ttf2woff2");
+const toWoff2 = require("gulp-ttftowoff2");
 
 let transform = () => {
 	return src(paths.fonts.src)
